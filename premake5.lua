88a5abb757c38c86040679ae6a5f5f8d8b70c827
Kienauer Rene <rene102@live.de>
2019-06-17 22:16:03 +0100

    Moved stuff

--- premake5.lua
+++ premake5.lua
@@ -7,7 +7,16 @@ project "IMGUI"
 
 	files
 	{
-        "include/"
+        "imconfig.h",
+		"imgui.cpp",
+		"imgui.h",
+		"imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h"
     } 
 
 	filter "system:windows"
