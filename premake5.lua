488175355d0e7f68e9d6a5147141613a2929813b
Kienauer Rene <rene102@live.de>
2019-06-17 06:30:41 +0100

    Fixed polluting files

---
+++ premake5.lua
@@ -0,0 +1,19 @@
+project "IMGUI"
+    kind "StaticLib"
+    language "C++"
+    
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+        "include/"
+    } 
+
+	filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+        
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
